2146682a4ffccee5e71719629d056ad191d67f87
rearcher <123781007@qq.com>
2020-07-08 10:50:25 +0800

    start

---
+++ src/test/java/org/csu/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.csu.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
