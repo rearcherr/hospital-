b9f94e66f1109bb068a723ec84f3d145a5d0e1c7
rearcher <123781007@qq.com>
2020-07-13 20:07:50 +0800

    添加了pagehelper

--- src/main/java/org/csu/hospital/controller/ManageMoneyController.java
+++ src/main/java/org/csu/hospital/controller/ManageMoneyController.java
@@ -1,5 +1,7 @@
 package org.csu.hospital.controller;
 
+import com.github.pagehelper.PageInfo;
+import org.csu.hospital.domain.Doctor;
 import org.csu.hospital.domain.ReturnAllPatientInfo;
 import org.csu.hospital.persistence.DoctorMapper;
 import org.csu.hospital.service.DoctorService;
@@ -7,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -19,8 +22,8 @@ public class ManageMoneyController {
     //查看所有医生工资
     @GetMapping("/salarys")
     @ResponseBody
-    public void getPatientInfo(){
-
+    public PageInfo<Doctor> getPatientInfo(@RequestParam("pageNum")int pageNum, @RequestParam("doctor_id")int pageSize){
+            return doctorService.findAllUserByPageS(pageNum,pageSize);
     }
 
 }
