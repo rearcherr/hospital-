fd9d6f27630f80a1fbb2d35ed07a833665e769a8
WindReiner <44196436+WindReiner@users.noreply.github.com>
2020-07-14 19:52:54 +0800

    修改了资金后台

---
+++ src/main/java/org/csu/hospital/domain/PurchaseRecord.java
@@ -0,0 +1,61 @@
+package org.csu.hospital.domain;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class PurchaseRecord {
+    int id;
+    int medId;
+    String name;
+    int amount;
+    BigDecimal price;
+    Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMedId() {
+        return medId;
+    }
+
+    public void setMedId(int medId) {
+        this.medId = medId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
--- src/main/java/org/csu/hospital/service/MedicineService.java
+++ src/main/java/org/csu/hospital/service/MedicineService.java
@@ -1,10 +1,15 @@
 package org.csu.hospital.service;
 
+import org.csu.hospital.domain.MedicalRecord;
+import org.csu.hospital.domain.Medicine;
+import org.csu.hospital.domain.PurchaseRecord;
 import org.csu.hospital.persistence.MedicineMapper;
+import org.csu.hospital.persistence.PatientMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class MedicineService {
@@ -12,8 +17,23 @@ public class MedicineService {
     @Autowired
     MedicineMapper medicineMapper;
 
-    public double getPriceByMedcineId(int id){
+    public double getPriceByMedcineId(int id) {
         return medicineMapper.getPriceByMedicineId(id);
+    }
+
+    public List<PurchaseRecord> getPurchaseRecordByPage(int pagenum, int pagesize) {
+        pagenum  = pagesize * pagenum;
+        List<PurchaseRecord> purchaseRecordList = medicineMapper.getPurchaseRecordByPage(pagenum,pagesize);
+        for (PurchaseRecord purchaseRecord:purchaseRecordList
+             ) {
+            System.out.println(purchaseRecord.getMedId());
+            purchaseRecord.setName((medicineMapper.getMedicineById(purchaseRecord.getMedId()).getMedName()));
+        }
+        return purchaseRecordList;
+    }
+
+    public int getPurchaseRecordPageNum() {
+        return (int)((medicineMapper.getPurchaseRecordNum())/10+1);
     }
 
 }
