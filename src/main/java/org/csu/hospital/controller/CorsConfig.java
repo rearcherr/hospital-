4a4f6729ec4d34ad475c48d8ae1e2cba9388d932
dolo2333 <dolo.liu@qq.com>
2020-07-15 16:05:45 +0800

    添加跨域配置

---
+++ src/main/java/org/csu/hospital/controller/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins(ALL).allowedMethods(ALL).allowedHeaders(ALL)
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
